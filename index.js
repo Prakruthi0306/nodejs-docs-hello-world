f28d4c6cc4e72af49474b2a933e23e50f01d0412
Prakruthi0306 <110379361+Prakruthi0306@users.noreply.github.com>
2024-04-21 23:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@
 // ***************************************************************************
 
 const express = require('express');    
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser'); 
 const cors = require('cors')
 const crypto = require('crypto');
 const pkg = require('./package.json');
